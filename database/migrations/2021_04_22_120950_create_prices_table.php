6b16b71fabaccd19025df9f09ce5ff27114bebc7
Baki Enes Yalçın <82122237+bakienes@users.noreply.github.com>
2021-04-23 00:45:15 +0300

    Add files via upload

---
+++ database/migrations/2021_04_22_120950_create_prices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->string('head',100);
+            $table->string('commenthead',50);
+            $table->decimal('price',50);
+            $table->string('comment',50); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
